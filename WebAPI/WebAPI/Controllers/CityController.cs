5e08854d13e338bd31d74ac780e43105e886c8a1
ilyushatrue <ilya.trukhin.99@mail.ru>
2023-06-28 14:46:10 +1000

    Add project files.

---
+++ WebAPI/WebAPI/Controllers/CityController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CityController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Atlanta", "New York" };
+        }
+        [HttpGet("{id}")]
+        public City GetCity(int id)
+        {
+            IEnumerable<City> cities = new List<City>()
+            {
+                new City { Id = 1, Name = "Atlanta"},
+                new City { Id = 2, Name = "New York"},
+            };
+            return cities.FirstOrDefault(c => c.Id == id);
+        }
+    }
+}
